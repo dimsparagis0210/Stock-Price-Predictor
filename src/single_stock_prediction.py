6978c2388a26f10f8decfe3129f77cb2cd2d795b
Dimitris Sparagis <dimsparagis@gmail.com>
2024-08-27 14:51:46 +0300

    Initialized a model for single stock prediction

---
+++ src/single_stock_prediction.py
@@ -0,0 +1,48 @@
+import pandas as pd
+
+# Exporting a new dataset for a single stock
+df = pd.read_csv('../data/World-Stock-Prices-Dataset.csv', sep = ";")
+ticker = 'AAPL' # Selected stock
+stock_data = df[df['Ticker'] == ticker] ## Filtering the dataset
+# Exporting to .csv
+stock_data.to_csv('../data/{Ticker}_Daily_Prices.csv'.format(Ticker = ticker), index = False)
+
+# Defining Features and the dependent variable
+X = df.iloc[:, :5]
+y = df.iloc[:, -1]
+
+# Encode the date
+# X['Day'] = pd.to_datetime(X['Date'], utc = True).dt.day
+# X['Month'] = pd.to_datetime(X['Date'], utc = True).dt.month
+# X['Year'] = pd.to_datetime(X['Date'], utc = True).dt.year
+del X['Date']
+
+# Splitting into train and test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+price_scaler = StandardScaler()
+X[['Open', 'High', 'Low']] = price_scaler.fit_transform(X[['Open', 'High', 'Low']])
+vol_scaler = StandardScaler()
+X[['Volume']] = vol_scaler.fit_transform(X[['Volume']])
+
+X[['Open', 'High', 'Low']] = price_scaler.inverse_transform(X[['Open', 'High', 'Low']])
+print(X)
+# Creating a polynomial Vector with every feature
+from sklearn.preprocessing import PolynomialFeatures
+poly_regressor = PolynomialFeatures(degree = 3)
+X_poly = poly_regressor.fit_transform(X)
+# Creating a linear regression model that will accept X polynom
+from sklearn.linear_model import LinearRegression
+lin_regressor = LinearRegression()
+lin_regressor.fit(X_poly, y)
+
+# Predicting price for a day of the month
+prices = price_scaler.transform([[227, 229, 225]]).reshape(-1, 1)
+volume = vol_scaler.transform([[200000]]).reshape(-1, 1)
+print(prices)
+print(volume)
+# prediction = lin_regressor.predict(poly_regressor.fit_transform([prices, volume]))
+# print(prediction)
